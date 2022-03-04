70fc8895054daefbe4be6b12cdd31898af6bad72
AttriGhosh96 <attrighosh@gmail.com>
2022-03-03 17:10:30 -0800

    Working on encoders

---
+++ config/regressor.py
@@ -0,0 +1,21 @@
+"""This python file contains the configuration dictionary with all the selctors, transformer and ML methods, 
+which all will be converted to genetic programming operators.
+
+The custom AUTOQTL configuration must be in nexted dictionary format, 
+where the first level key is the path and name of the operator and the 
+second level key is the corresponding parameter name for that operator. 
+The second level key should point to a list of parameter values for that parameter. """
+
+import numpy as np
+
+# Feature Selectors
+
+
+
+
+# Feature Transformers (Encoders)
+
+
+
+
+# Machine Learning Methods
