f2f775c71ff8de6b36c30114f02b41074badf34b
AttriGhosh96 <attrighosh@gmail.com>
2022-03-04 17:26:05 -0800

    Feature selection based on allele frequency

--- config/regressor.py
+++ config/regressor.py
@@ -1,7 +1,7 @@
 """This python file contains the configuration dictionary with all the selctors, transformer and ML methods, 
 which all will be converted to genetic programming operators.
 
-The custom AUTOQTL configuration must be in nexted dictionary format, 
+The custom AUTOQTL configuration must be in nested dictionary format, 
 where the first level key is the path and name of the operator and the 
 second level key is the corresponding parameter name for that operator. 
 The second level key should point to a list of parameter values for that parameter. """
